8d981d0b1cd0ccec2d024f37a60830ef449c7511
Daniel Fernandez <danielifg70@gmail.com>
2021-05-17 20:04:09 -0400

    css index

--- js/Galeria_Dinamica.js
+++ js/Galeria_Dinamica.js
@@ -5,17 +5,22 @@ $(document).ready(function () {
         var combo = document.getElementById("categoria");
         var tipo_cotegoria = combo.options[combo.selectedIndex].text;
 
-        if (tipo_cotegoria == 'Cuerpo Completo') {
+        if (tipo_cotegoria == 'Seleccione una Categoria') {
+            $("#tabla-categorias").empty();
+            // var fila = "<thead><tr><th scope='col'></th><th scope='col'> Id </th><th scope='col'> Gato </th><th scope='col'> Nombre </th><th scope='col'> Descripción</th></tr></thead>"
+            // $("#tabla-categorias").append(fila);
+
+        } else if (tipo_cotegoria == 'Cuerpo Completo') {
             $("#tabla-categorias").empty();
             var fila = "<thead><tr><th scope='col'></th><th scope='col'> Id </th><th scope='col'> Gato </th><th scope='col'> Nombre </th><th scope='col'> Descripción</th></tr></thead>"
             $("#tabla-categorias").append(fila);
-            var fila = "<tr><td>  </td><td> 1 </td>  <td> <img src='../images/Eusebia.png'> <td> Eusebia </td><td> Eusebia es una gatita que fue rescatada en un chat en el año 2007, si la rescatas no tendrás mas ratones en tu casa, ya que ella los debora. </td></tr>"
+            var fila = "<tr><td>  </td><td> 1 </td>  <td> <img class='pequeña' src='../images/Eusebia.png'> <td> Eusebia </td><td> Eusebia es una gatita que fue rescatada en un chat en el año 2007, si la rescatas no tendrás mas ratones en tu casa, ya que ella los debora. </td></tr>"
             $("#tabla-categorias").append(fila);
-            var fila = "<tr><td>  </td><td> 2 </td> <td> <img src='../images/Rallita.png'> <td> Rallita </td><td> Rallita es una fiel gata guardiana, estará todo el día vigilando que no le pase nada a tu hogar, incluso vigila mas que la tipica vecina del barrio. </td></tr>"
+            var fila = "<tr><td>  </td><td> 2 </td> <td> <img class='pequeña' src='../images/Rallita.png'> <td> Rallita </td><td> Rallita es una fiel gata guardiana, estará todo el día vigilando que no le pase nada a tu hogar, incluso vigila mas que la tipica vecina del barrio. </td></tr>"
             $("#tabla-categorias").append(fila);
-            var fila = "<tr><td>  </td><td> 3 </td><td> <img src='../images/Carlitos.png'> <td> Jerry </td><td> Jerry es un gato triste que fue abandonado en yahoo respuestas. Muchos gatos le hacen bullying por tener nombre de ratón, es por eso que Jerry te necesita mas que nunca. </td></tr>"
+            var fila = "<tr><td>  </td><td> 3 </td><td> <img class='pequeña' src='../images/Carlitos.png'> <td> Jerry </td><td> Jerry es un gato triste que fue abandonado en yahoo respuestas. Muchos gatos le hacen bullying por tener nombre de ratón, es por eso que Jerry te necesita mas que nunca. </td></tr>"
             $("#tabla-categorias").append(fila);
-            var fila = "<tr><td>  </td><td> 4 </td><td> <img src='../images/Jerry.png'> <td> Carlitos </td><td> Carlitos... un gato feliz que fue desechado, aun así, no pierde la sonrisa en su cara esperando que lo rescates y lo utilices en algún foro. </td></tr>"
+            var fila = "<tr><td>  </td><td> 4 </td><td> <img class='pequeña' src='../images/Jerry.png'> <td> Carlitos </td><td> Carlitos... un gato feliz que fue desechado, aun así, no pierde la sonrisa en su cara esperando que lo rescates y lo utilices en algún foro. </td></tr>"
             $("#tabla-categorias").append(fila);
 
 
