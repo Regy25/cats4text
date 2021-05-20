26f3c6ea4dc68d4b89f859b2c5efdb2637c20bd7
Daniel Fernandez <danielifg70@gmail.com>
2021-05-20 17:55:36 -0400

    Cambio en submit

--- js/form_submit.js
+++ js/form_submit.js
@@ -1,38 +1,40 @@
-$(document).ready(function() {
-    $("#contact-form").validate({
-      rules: {
-        nombre_gato: {
-          required: true,
-          minlength: 2
-        },
-        historia_gato: {
-            required: true,
-            minlength: 6
-        },
-        category:{
-            required: true,
-        },
-        gato: {
-            required: true,
-            minlength: 2
-        }
+$(document).ready(function () {
+  $("#contact-form").validate({
+    rules: {
+      nombre_gato: {
+        required: true,
+        minlength: 2
       },
-      messages : {
-        nombre_gato: {
-          required: "Ingrese Nombre",
-          minlength: "Largo minimo 3 caracteres"
-        },
-        historia_gato: {
-            required: "Ingrese Historia",
-            minlength: "Largo minimo 6 caracteres"
-        },
-        category: {
-            required: "Escoja una categoria",
-        },
-        gato: {
-            required: "Ingresa el gatito",
-            minlength: "Largo min 2 caracteres"
-        }
+      historia_gato: {
+        required: true,
+        minlength: 6
+      },
+      category: {
+        required: true,
+      },
+      gato: {
+        required: true,
+        minlength: 2
       }
-    });
+    },
+    messages: {
+      nombre_gato: {
+        required: "Ingrese Nombre",
+        minlength: "Largo minimo 3 caracteres"
+      },
+      historia_gato: {
+        required: "Ingrese Historia",
+        minlength: "Largo minimo 6 caracteres"
+      },
+      category: {
+        required: "Escoja una categoria",
+      },
+      gato: {
+        required: "Ingresa el gatito",
+        minlength: "Largo min 2 caracteres"
+      }
+    },
+      errorElement: 'div'
+    
   });
+});
